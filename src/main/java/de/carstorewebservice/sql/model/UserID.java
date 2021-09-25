51f8c8b78f45d323740a9adb7e1af8f278f215a3
magantus@googlemail.com <Extrem93!>
2021-09-26 01:51:32 +0200

    add user to sql/model

---
+++ src/main/java/de/carstorewebservice/sql/model/UserID.java
@@ -0,0 +1,12 @@
+package de.carstorewebservice.sql.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserID {
+    long id;
+}
