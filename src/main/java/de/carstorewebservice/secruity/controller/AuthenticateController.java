e8278f3f56d8e78db099db56ed7d108593eec249
magantus@googlemail.com <Extrem93!>
2021-09-25 21:48:57 +0200

    add Model, Controller add Security dep

---
+++ src/main/java/de/carstorewebservice/secruity/controller/AuthenticateController.java
@@ -0,0 +1,47 @@
+package de.carstorewebservice.secruity.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class AuthenticateController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Daily Code Buffer!!";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            return  new JwtResponse(e.getMessage());
+        }
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+        final String token =
+                jwtUtility.generateToken(userDetails);
+        return  new JwtResponse(token);
+    }
+}
